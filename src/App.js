84d1104d785550f0c245f8d7ce70b6896906f137
Traneisha Sanford <mrs.sanford@Traneishas-MacBook-Air.local>
2019-09-02 12:42:15 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './App.css'
+
+import Timeline from './timeline';
+
+// Leaving this in from create-react-app
+// All new login lives in the /components folder
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Timeline />
     </div>
   );
 }
